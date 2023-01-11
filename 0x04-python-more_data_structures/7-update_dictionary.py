e74e70b8ddd9b0cc6065b28988ba411d505a2a89
BosiboriMel <bosiborimelvin609@gmail.com>
2023-01-11 06:14:48 -0800

    task 8 0x04 python

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,2 @@
-#!/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return (a_dictionary)
+#!/bin/python3 def update_dictionary(a_dictionary, key, value):
+a_dictionary[key] = value   return (a_dictionary)
